65cd2c4f46063375ab7b213fec8124b831fbd943
Robin Frugte <robin.frugte@hotmail.com>
2020-01-22 12:59:50 +0100

    Merged metadata dataset with daily top 100 dataset.

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,27 @@
 const countries = ['at', 'ec', 'hk', 'fi', 'lv', 'it', 'jp', 'nl', 'nz', 'tr']
 const trackSelection = ["1","2","3","4","5"]
 let data = []
+let metaData = []
 
 async function init(){
-    await getData(data)
+    await getData()
     mapData(data)
     console.log(mapData(data))
+    console.log(metaData)
 }
 
-async function getData(data){
+async function getData(){
     await d3.csv("../data/data.csv", entry => {
         return data.push(entry)
     })
-    return data
+    await d3.csv("../data/top2017.csv", entry => {
+        return metaData.push(entry)
+    })
 }
 
 function mapData(data){
-    let reMap = data.reduce((newData, entry) => {
+    let reMap = data.reduce((newData, entry, i) => {
+        let findMetaData = metaData.find(el => entry['Track Name'] == el.name)
         const countryObject = {
             countryCode: entry.Region,
             totalAmountPlayed: Number(entry.Streams),
@@ -33,11 +38,15 @@ function mapData(data){
         const foundObject = newData.find(el => {
             return el.countryCode === entry.Region
         })
+        
         if (!foundObject && trackSelection.some(el => entry.Position == el) && countries.some(el => entry.Region.includes(el))) {
             newData.push(countryObject)
-        } else if (trackSelection.some(el => entry.Position == el) && countries.some(el => entry.Region.includes(el))) {
+        } else if (trackSelection.some(el => entry.Position == el) && countries.some(el => entry.Region.includes(el)) && findMetaData != undefined) {
             foundObject.tracks.push(musicObject)
             foundObject.totalAmountPlayed += musicObject.amountPlayed
+            musicObject.length = findMetaData.duration_ms
+            musicObject.bpm = findMetaData.tempo
+            musicObject.danceability = findMetaData.danceability
         }
         return newData
     }, [])
