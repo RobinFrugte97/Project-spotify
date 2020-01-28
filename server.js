1beb3ac78496f908c88c4f5a73e8658164a5d7ad
Robin Frugte <robin.frugte@hotmail.com>
2020-01-28 12:47:21 +0100

    removed server file from gitignore

---
+++ server.js
@@ -0,0 +1,5 @@
+let connect = require('connect');
+let serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function () {
+    console.log('Server running on 8080...');
+})
